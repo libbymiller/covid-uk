b83f18d238b79f8b60c8c2cee3e4f15215715ca6
Kristian Zarebski <kristian.zarebski@ukaea.uk>
2020-05-07 16:24:27 +0100

    Added file rename to Bash script

--- run_all.sh
+++ run_all.sh
@@ -1,3 +1,7 @@
-echo "==== Running Covid-UK simulation with 1 stochastic realisation for testing ===="
+export CURRENT_BRANCH=$(git symbolic-ref --short -q HEAD)
+
+echo "==== Running Covid-UK simulation on branch '$CURRENT_BRANCH' with 1 stochastic realisation for testing ===="
 for i in 1 2.1 2.2 3 4 5 6; do echo "==== Running Stage $i ====" && Rscript UK.R $i 1; done
+rename .qs ${CURRENT_BRANCH}.qs *.qs 
 nosetests .
+
